1858f2d86c6edf13df880220b40fb9f13bf1f046
Ruben Diaz <ruben.diaz@tamedia.ch>
2017-09-22 14:34:14 +0200

    trying db read

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { moveIn, fallIn } from '../router.animations';
 
 @Component({
@@ -14,19 +17,46 @@ export class SignupComponent implements OnInit {
 
   state: string = '';
   error: any;
+  codes;
+  formData;
 
   constructor(public af: AngularFire,private router: Router) {
 
   }
 
+
   onSubmit(formData) {
     if(formData.valid) {
-      console.log(formData.value);
+      this.formData = formData;
+      console.log('formData.value: ' + formData.value);
+
+      var codes$: FirebaseListObservable<any[]>;
+
+      this.codes = this.af.database.list('Codes',{preserveSnapshot:true});
+      this.codes.subscribe(snapshots => {
+        snapshots.forEach(snapshot => {
+            console.log(snapshot.val().code);
+            //this.agencyID.push(snapshot.val()._id);
+
+        });
+
+    });
+
+
+
+
+
+
+
+
       this.af.auth.createUser({
         email: formData.value.email,
         password: formData.value.password
       }).then(
         (success) => {
+
+          console.log(success);
+
         this.router.navigate(['/members'])
       }).catch(
         (err) => {
@@ -36,6 +66,11 @@ export class SignupComponent implements OnInit {
   }
 
   ngOnInit() {
+      this.codes = this.af.database.list('/Codes', {
+        query: {
+            orderByChild: 'createdAt'
+        }
+      });
   }
 
 }
