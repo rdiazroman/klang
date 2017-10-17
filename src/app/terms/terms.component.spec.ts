25345e4e825f40dbc00a2d9d067f6c2d5639849c
Ruben Diaz <ruben.diaz@tamedia.ch>
2017-10-17 15:07:33 +0200

    styling and terms (not finished)

--- src/app/terms/terms.component.spec.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-
-describe('TermsComponent', () => {
-
-});
