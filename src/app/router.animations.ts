34c14a38aad7a1eadbe5da447f4134a429c4b1c0
Ruben Diaz <ruben.diaz@tamedia.ch>
2017-11-08 15:06:29 +0100

    puto scroll

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -2,8 +2,8 @@ import {trigger, state, animate, style, transition} from '@angular/core';
 
 export function moveIn() {
   return trigger('moveIn', [
-    state('void', style({position: 'fixed', width: '100%'}) ),
-    state('*', style({position: 'fixed', width: '100%'}) ),
+    state('void', style({position: 'absolute', width: '100%'}) ),
+    state('*', style({position: 'absolute', width: '100%'}) ),
     transition(':enter', [
       style({opacity:'0', transform: 'translateX(100px)'}),
       animate('.6s ease-in-out', style({opacity:'1', transform: 'translateX(0)'}))
